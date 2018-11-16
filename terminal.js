9428b6e7602e58f11cc07f15bd32869149e8f2ed
ttzztztz <935319340@qq.com>
2018-11-16 22:58:18 +0800

    mkdir and touch

--- terminal.js
+++ terminal.js
@@ -1,28 +1,46 @@
 const terminalReg = /\s*(".+?"|[^:\s])+((\s*:\s*(".+?"|[^\s])+)|)|(".+?"|[^"\s])+/g;
-function Method(){
+
+function Method() {
 
 }
 
-Method.prototype.cat = function(route){
+Method.prototype.cat = function (route) {
     let file = new File();
     return file.read(route);
 };
 
+Method.prototype.mkdir = function(route){
+    let directory = new Directory();
+    let arr = route.split("/");
+    let dirname = arr[arr.length -1];
+    arr.pop();
+    let path = "/" + arr.join("/");
+    return Directory.create(path , dirname);
+};
+
+Method.prototype.touch = function(route){
+    let file = new File();
+    let time = new Time();
+    return file.changeDate(route,time.time());
+};
+
 let method = new Method();
-function _Terminal(){
+
+function _Terminal() {
 
 }
-_Terminal.prototype.input = function(data){
+
+_Terminal.prototype.input = function (data) {
     let arr = data.match(terminalReg);
-    arr = arr.map(function(item,index,array){
+    arr = arr.map(function (item, index, array) {
         return item.trim();
     });
     let command = arr[0];
     arr.shift();
     let args = "";
-    arr.forEach(function(item,index,array){
-        if(index!==0) args+=",";
-        args = '"'+ item + '"';
+    arr.forEach(function (item, index, array) {
+        if (index !== 0) args += ",";
+        args = '"' + item + '"';
     });
-    return eval("method."+command+"("+args+")");
+    return eval("method." + command + "(" + args + ")");
 };
